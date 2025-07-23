f5479a66b25fafcfce8ba9a81922517555ca552c
Ahmed Kadiwala <127075303+AhmedKadiwala@users.noreply.github.com>
2025-07-23 15:50:13 +0530

    Update 01_reading_files.py

--- 09_File_IO/chapter 9/01_reading_files.py
+++ 09_File_IO/chapter 9/01_reading_files.py
@@ -1,10 +1,18 @@
-import PyPDF2
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
 
-# Open the PDF in binary mode
-with open("C:/Users/Ahmed Kadiwala/Desktop/resume(2.0.2).pdf", "rb") as file:
-    reader = PyPDF2.PdfReader(file)
+
+
+
+# import PyPDF2
+
+# # Open the PDF in binary mode
+# with open("C:/Users/Ahmed Kadiwala/Desktop/resume(2.0.2).pdf", "rb") as file:
+#     reader = PyPDF2.PdfReader(file)
     
-    # Extract text from all pages
-    for i, page in enumerate(reader.pages):
-        text = page.extract_text()
-        print(f"Page {i + 1}:\n{text}\n")
+#     # Extract text from all pages
+#     for i, page in enumerate(reader.pages):
+#         text = page.extract_text()
+#         print(f"Page {i + 1}:\n{text}\n")
